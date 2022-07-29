b67aa9796ee57dd9f2969e484c5fca820db89a7f
uzzy11 <muhammed.usman77@gmail.com>
2022-07-29 14:15:37 +0500

    Firebase integrated

---
+++ firebase.js
@@ -0,0 +1,6 @@
+import firebase from '@react-native-firebase/app'
+import auth from '@react-native-firebase/auth'
+import realdb from '@react-native-firebase/database'
+import db from '@react-native-firebase/firestore'
+
+export {auth, db, realdb}
